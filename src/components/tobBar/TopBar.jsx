5ab1ed1f841c627875f531f68c745176febde5ed
Akash Behara <akashbehara33@gmail.com>
2021-12-25 18:40:10 +0530

    initial prject setup

---
+++ src/components/tobBar/TopBar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './TopBar.css';
+
+export default function TopBar() {
+    return (
+        <div className='topbar'>
+            <div className="topbarWrapper">
+                
+              <div className="topLeft">
+                  <span className='logo'>Admin</span>
+              </div>
+
+              <div className="topRight">
+                  <div className="topbarIcons">
+
+                  </div>
+              </div>
+            </div>  
+        </div>
+    )
+}
