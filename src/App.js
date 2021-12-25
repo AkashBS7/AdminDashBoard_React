bd49362912bef4c0e3a556b3cc66178637d86483
Akash Behara <akashbehara33@gmail.com>
2021-12-25 23:47:00 +0530

    user

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Sidebar from "./components/sidebar/Sidebar";
 import TopBar from "./components/tobBar/TopBar";
 
 import './app.css';
 import Home from "./pages/homePage/Home";
+import UserList from "./pages/Userlist/UserList";
+import User from "./pages/user/User";
+import Newuser from "./pages/newUser/Newuser";
 
 function App() {
   return (
-    <div>
+    <Router>
       <TopBar />
       <div className="container">
         <Sidebar />
-        {/* <div className="others">other Pages</div> */}
-        <Home />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+
+          <Route path='/users'>
+            <UserList />
+          </Route>
+
+          <Route path='/user/:userId'>
+            <User />
+          </Route>
+
+          <Route path='/newUser'>
+            <Newuser />
+          </Route>
+
+        </Switch>        
       </div>
-    </div>
+    </Router>
   );
 }
 
